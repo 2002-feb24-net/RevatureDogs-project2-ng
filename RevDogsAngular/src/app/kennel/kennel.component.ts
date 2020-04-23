29d11d96d86e866d96fc3811bc62bab99f08ca6c
Russell Gehan <rdgehan@gmail.com>
2020-04-23 14:44:35 -0500

    Formatted how dogs appear in kennel

--- RevDogsAngular/src/app/kennel/kennel.component.ts
+++ RevDogsAngular/src/app/kennel/kennel.component.ts
@@ -3,6 +3,8 @@ import { Users } from '../models/users';
 import { UsersService } from '../users.service';
 import { Dogs } from '../models/dogs';
 import { DogsService } from '../dogs.service';
+import { DogType } from '../models/dog-types';
+import { DogTypesService } from '../dog-types.service';
 
 @Component({
   selector: 'app-kennel',
@@ -13,6 +15,7 @@ export class KennelComponent implements OnInit {
   loggedUser: Users;
   dogs: Dogs[];
   selectedDog: Dogs;
+  breeds: DogType[];
 
   constructor(private usersService: UsersService, private dogsService: DogsService) { }
 
@@ -25,4 +28,31 @@ export class KennelComponent implements OnInit {
     this.selectedDog = dog;
   }
 
+  getHunger(dog: Dogs): string{
+    var hunger = "";
+    for(var i = 0; i < 10; i++){
+      hunger += (dog.hunger >= (i*10)) ? "🍖": "🦴";
+    }
+    return hunger;
+  }
+
+  getMood(dog: Dogs): string{
+    if(dog.mood > 80)
+      return "😃";
+    else if(dog.mood > 60)
+      return "🙂";
+    else if(dog.mood > 40)
+      return "😐";
+    else if(dog.mood > 20)
+      return "☹️";
+    else return "😭";
+  }
+
+  getEnergy(dog: Dogs): string{
+    var energy = "";
+    for(var i = 0; i < dog.energy; i++){
+      energy += "⚡";
+    }
+    return energy;
+  }
 }
